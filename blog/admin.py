58321c79ec58b2692db6cf6b007fd70182f94240
mostafa-asefy <asefi.v@gmail.com>
2020-06-20 19:28:17 +0330

    After l02, Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'publish')
+    list_filter = ('status', 'publish', 'create')
+    search_fields = ('title', 'body')
+    ordering = ('status', 'publish')
+    date_hierarchy = 'publish'
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
