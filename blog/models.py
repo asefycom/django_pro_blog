acb78ea80c4919ee4fefba1b5480b2130e501fd3
mostafa-asefy <asefi.v@gmail.com>
2020-06-20 17:15:30 +0330

    After L02, Blog app

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (('draft', 'Draft'),('published', 'Published'),)
+    title = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250, unique_for_date='publish')
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
