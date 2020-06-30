52193654004b0f07e06c097783015cf6d39b0df5
mostafa-asefy <asefi.v@gmail.com>
2020-06-30 14:09:45 +0330

    After L05, Manager

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (('draft', 'Draft'),('published', 'Published'),)
     title = models.CharField(max_length=250)
@@ -13,6 +18,9 @@ class Post(models.Model):
     update = models.DateTimeField(auto_now=True)
     publish = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
