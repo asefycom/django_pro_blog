3871880de7ae2f101a73dcffe083e58a813e28c7
mostafa-asefy <asefi.v@gmail.com>
2020-08-16 18:55:37 +0330

    L22&23: SQLite to PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'mostafa',
+        'PASSWORD': 'mostafa123456',
     }
 }
 
