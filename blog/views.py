d4d849081bcf4a931ece1a79d66c8ff386ef9860
mostafa-asefy <asefi.v@gmail.com>
2020-08-17 12:08:22 +0330

    L25: Advanced Search Engine

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
@@ -108,9 +108,39 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
           query = form.cleaned_data['query']
+
+
+        #Step 1
+          # results = Post.published.annotate(
+          #     search=SearchVector('title', 'body'),
+          # ).filter(search=query)
+
+        #Step 2
+          # search_vector = SearchVector('title', 'body')
+          # search_query = SearchQuery(query)
+          # results = Post.published.annotate(
+          #     search=search_vector, rank=SearchRank(search_vector, search_query)
+          # ).filter(search=search_query).order_by('-rank')
+
+          search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+          search_query = SearchQuery(query)
           results = Post.published.annotate(
-            search=SearchVector('title', 'body'),
-            ).filter(search=query)
+            rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
